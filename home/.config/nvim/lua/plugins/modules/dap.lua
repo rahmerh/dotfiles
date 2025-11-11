03bfaffd488fa31cd99bc5be0bbb4459a19addc0
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-11-11 09:20:54 +0100

    Load dap ui when using dap

--- home/.config/nvim/lua/plugins/modules/dap.lua
+++ home/.config/nvim/lua/plugins/modules/dap.lua
@@ -13,6 +13,20 @@ return {
         config = function()
             require("debug.bash")
             require("debug.rust")
+
+            local dap, dapui = require("dap"), require("dapui")
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
         end,
     },
     {
