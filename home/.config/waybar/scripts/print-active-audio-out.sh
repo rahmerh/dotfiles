451dc51e9b2d8a7b4ceb8bf628cb8568f3a2ddd9
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-11-14 10:33:37 +0100

    Added headphones to waybar

--- home/.config/waybar/scripts/print-active-audio-out.sh
+++ home/.config/waybar/scripts/print-active-audio-out.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-declare -A known_sinks
-known_sinks["AudioRelay-Virtual-Speaker"]="Audiorelay"
-known_sinks["Built-in Audio Analog Stereo"]="Speakers"
+declare -A known_sinks=(
+    ["audiorelay_Speaker"]="Audiorelay"
+    ["alsa_output.pci-0000_00_1f.3.analog-stereo"]="Speakers"
+    ["alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"]="Headphones"
+)
 
 get_sink_name() {
-    active_sink=$(pactl list sinks | awk '$1 == "State:" && $2 == "RUNNING" { in_running_sink = 1 } in_running_sink && $1 == "Description:" { print substr($0, index($0,$2)); exit }')
+    local sink desc
+    sink=$(pactl get-default-sink 2>/dev/null) || return
 
-    if [[ -n "${known_sinks[$active_sink]}" ]]; then
-        echo "${known_sinks[$active_sink]}"
+    if [[ -n "${known_sinks[$sink]}" ]]; then
+        echo "${known_sinks[$sink]}"
     else
-        echo "$active_sink"
+        echo "$sink"
     fi
 }
 
-pactl subscribe | grep --line-buffered "Event 'change' on sink" | while read -r _; do
-    get_sink_name
-done
-
 get_sink_name
+
+pactl subscribe \
+  | grep --line-buffered -E "Event 'change' on (sink|server)|Event 'new' on sink" \
+  | while read -r _; do
+        get_sink_name
+    done
--- home/.config/waybar/scripts/toggle-audio-out.sh
+++ home/.config/waybar/scripts/toggle-audio-out.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-audiorelay="audiorelay-out"
+audiorelay="audiorelay_Speaker"
 speakers="alsa_output.pci-0000_00_1f.3.analog-stereo"
+headphones="alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"
 
 current_sink=$(pactl get-default-sink)
 
 if [[ "$current_sink" == "$audiorelay" ]]; then
     pactl set-default-sink "$speakers"
 elif [[ "$current_sink" == "$speakers" ]]; then
+    pactl set-default-sink "$headphones"
+elif [[ "$current_sink" == "$headphones" ]]; then
     pactl set-default-sink "$audiorelay"
 fi
-
