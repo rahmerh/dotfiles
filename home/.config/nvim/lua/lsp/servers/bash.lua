bee81e2be249064efada20bea7c1210c7ca87b80
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-11-28 13:47:27 +0100

    Removed deprecated code

--- home/.config/nvim/lua/lsp/servers/bash.lua
+++ home/.config/nvim/lua/lsp/servers/bash.lua
@@ -1,8 +1,1 @@
-local lspconfig = require("lspconfig")
-local config = require("lsp.config")
-
-lspconfig.bashls.setup({
-    on_attach = config.on_attach,
-    capabilities = config.capabilities,
-    filetypes = { "sh", "bash" },
-})
+vim.lsp.enable('bashls')
