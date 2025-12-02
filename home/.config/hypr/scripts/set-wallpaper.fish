1752fec1d3bfec61aa8b2cbc2047705093590608
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-12-02 20:11:42 +0100

    Set wallpaper properly now

---
+++ home/.config/hypr/scripts/set-wallpaper.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+set base_dir ~/Pictures
+
+pkill hyprpaper >/dev/null
+hyprctl dispatch exec hyprpaper >/dev/null
+
+sleep 0.3
+
+for line in (hyprctl monitors -j | jq -r '.[] | "\(.name) \(.width)x\(.height)"')
+    set name (echo $line | awk '{print $1}')
+    set res (echo $line | awk '{print $2}')
+
+    set wp "$base_dir/wallpaper-$res.png"
+
+    if test -f "$wp"
+        hyprctl hyprpaper preload "$wp" >/dev/null
+        hyprctl hyprpaper wallpaper "$name,$wp" >/dev/null
+    else
+        if not contains -- "$res" $missing_resolutions
+            set missing_resolutions $missing_resolutions $res
+            notify-send "Missing wallpaper" "No wallpaper found for $res ($wp)"
+        end
+    end
+end
--- install-scripts/20-themes/02-generate-wallpaper.fish
+++ install-scripts/20-themes/02-generate-wallpaper.fish
@@ -1,22 +1,43 @@
 #!/usr/bin/env fish
 source install-scripts/library/print-utils.fish
 
-print_info "Creating wallpaper"
+function make_wallpaper
+    set -l width $argv[1]
+    set -l height $argv[2]
+    set -l out $argv[3]
 
-set bg "#2C2C2C"
-set pink "#E85A98"
-set out ~/Pictures/wallpaper.png
-set tmp (mktemp -d)
+    set -l bg "#2C2C2C"
+    set -l pink "#E85A98"
+    set -l tmp (mktemp -d)
 
-magick -size 5120x1440 xc:"$bg" miff:"$tmp/canvas.miff"
+    magick -size {$width}x{$height} xc:"$bg" miff:"$tmp/canvas.miff"
 
-magick -size 5120x1440 xc:none -fill "$pink" -draw "polygon 0,0 200,0 500,1440 300,1440" miff:"$tmp/band1.miff"
-magick -size 5120x1440 xc:none -fill "$pink" -draw "polygon 300,0 400,0 700,1440 600,1440" miff:"$tmp/band2.miff"
-magick -size 5120x1440 xc:none -fill "$pink" -draw "polygon 500,0 600,0 900,1440 800,1440" miff:"$tmp/band3.miff"
+    magick -size {$width}x{$height} xc:none -fill "$pink" \
+        -draw "polygon 0,0 200,0 500,$height 300,$height" \
+        miff:"$tmp/band1.miff"
 
-magick "$tmp/canvas.miff" "$tmp/band1.miff" -compose Over -composite miff:"$tmp/step1.miff"
-magick "$tmp/step1.miff" "$tmp/band2.miff" -compose Over -composite miff:"$tmp/step2.miff"
-magick "$tmp/step2.miff" "$tmp/band3.miff" -compose Over -composite "$out"
+    magick -size {$width}x{$height} xc:none -fill "$pink" \
+        -draw "polygon 300,0 400,0 700,$height 600,$height" \
+        miff:"$tmp/band2.miff"
+
+    magick -size {$width}x{$height} xc:none -fill "$pink" \
+        -draw "polygon 500,0 600,0 900,$height 800,$height" \
+        miff:"$tmp/band3.miff"
+
+    magick "$tmp/canvas.miff" "$tmp/band1.miff" -compose Over -composite miff:"$tmp/step1.miff"
+    magick "$tmp/step1.miff" "$tmp/band2.miff" -compose Over -composite miff:"$tmp/step2.miff"
+    magick "$tmp/step2.miff" "$tmp/band3.miff" -compose Over -composite "$out"
+
+    rm -rf "$tmp"
+end
+
+print_info "Creating wallpapers"
+
+set main_out ~/Pictures/wallpaper-5120x1440.png
+
+make_wallpaper 5120 1440 $main_out
+make_wallpaper 2560 1600 ~/Pictures/wallpaper-2560x1600.png
+make_wallpaper 2560 1440 ~/Pictures/wallpaper-2560x1440.png
 
 pkill hyprpaper
 hyprctl dispatch exec hyprpaper
