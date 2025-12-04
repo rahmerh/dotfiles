4da687a9bedbb99c8aa831dd13820be0ab5fb013
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-12-04 08:50:45 +0100

    Install flutter-tools

--- home/.config/nvim/lua/plugins/modules/lsp.lua
+++ home/.config/nvim/lua/plugins/modules/lsp.lua
@@ -84,5 +84,17 @@ return {
                 },
             }
         end,
+    },
+
+    {
+        'nvim-flutter/flutter-tools.nvim',
+        lazy = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = function()
+            require("flutter-tools").setup()
+        end,
     }
 }
