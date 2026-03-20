2b10f506a3cc293af98af47f3cf473659bf1c5a6
Bas Bouwhuis <basbouwhuis@protonmail.com>
2026-03-20 17:05:21 +0100

    Copy screenshot icon in theming script

--- install-scripts/20-theming/03-fetch-icons.fish
+++ install-scripts/20-theming/03-fetch-icons.fish
@@ -2,12 +2,15 @@
 source install-scripts/library/download-utils.fish
 source install-scripts/library/print-utils.fish
 
-print_info "Downloading icons"
-
 set -l icon_dir ~/.local/share/icons
 
-download_favicon nexusmods.com /tmp/nexusmods.ico
+print_info "Downloading icons"
 
-magick /tmp/nexusmods.ico "$icon_dir/nexusmods.png"
+download_favicon nexusmods.com /tmp/nexusmods.ico
+magick /tmp/nexusmods.ico "$icon_dir/nexusmods.png" &>/dev/null
+
+print_info "Moving icons from dotfiles"
+
+cp ~/dotfiles/install-scripts/assets/icons/screenshot.png "$icon_dir"
 
 print_success Done
