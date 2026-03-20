65d11e87f5da4c1167f7eb848badfeced71b4a75
Bas Bouwhuis <basbouwhuis@protonmail.com>
2026-03-20 09:04:00 +0100

    Use treesitter correctly

--- home/.config/nvim/lua/plugins/modules/treesitter.lua
+++ home/.config/nvim/lua/plugins/modules/treesitter.lua
@@ -4,31 +4,20 @@ return {
         build = ":TSUpdate",
         lazy = false,
         config = function()
-            require("nvim-treesitter.configs").setup({
-                ensured_installed = {
-                    "bash",
-                    "comment",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "regex",
-                    "toml",
-                    "typescript",
-                    "yaml",
-                    "c_sharp",
-                    "go",
-                    "rust"
-                },
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = true,
-                },
-                indent = {
-                    enable = true
-                },
-                sync_install = true
-            });
+            require('nvim-treesitter').install({
+                'bash',
+                'comment',
+                'json',
+                "lua",
+                "markdown",
+                "regex",
+                "toml",
+                "typescript",
+                "yaml",
+                "c_sharp",
+                "go",
+                "rust"
+            })
         end,
     },
 }
