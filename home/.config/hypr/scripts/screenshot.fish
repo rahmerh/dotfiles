3826d591742b256dda751b1fc6f3510e14a66287
Bas Bouwhuis <basbouwhuis@protonmail.com>
2026-03-20 10:35:29 +0100

    Improve screenshot flow

---
+++ home/.config/hypr/scripts/screenshot.fish
@@ -0,0 +1,46 @@
+#!/usr/bin/env fish
+
+set -l screenshots_dir "$HOME/Pictures/Screenshots"
+set -l timestamp_now (date +'%Y-%m-%d_%H-%M-%S')
+set -l file "$screenshots_dir/$timestamp_now.png"
+
+mkdir -p "$screenshots_dir"
+
+function usage
+    echo "Usage: screenshot.fish <save|copy|both>"
+    exit 1
+end
+
+if test (count $argv) -ne 1
+    usage
+end
+
+set action $argv[1]
+
+set geom (slurp -b 00000088 -s 00000000 -w 0)
+if test -z "$geom"
+    exit 0
+end
+
+switch $action
+    case save
+        grim -g "$geom" "$file"
+
+        set -l clicked (notify-send \
+    --action=default=Open \
+    "Screenshot saved" \
+    "$file")
+
+        if test "$clicked" = default
+            xdg-open "$file" >/dev/null 2>&1 &
+        end
+    case copy
+        grim -g "$geom" - | wl-copy --type image/png --foreground
+        notify-send "Screenshot copied" "Copied to clipboard"
+    case both
+        grim -g "$geom" "$file"
+        wl-copy --type image/png --foreground <"$file"
+        notify-send "Screenshot saved + copied" $file
+    case '*'
+        usage
+end
