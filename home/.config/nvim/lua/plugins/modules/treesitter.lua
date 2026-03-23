aa83e06adadb10c9f1898f9b25cbc74d1410a0ab
Bas Bouwhuis <basbouwhuis@protonmail.com>
2026-03-23 08:50:33 +0100

    Force usage of treesitter main

--- home/.config/nvim/lua/plugins/modules/treesitter.lua
+++ home/.config/nvim/lua/plugins/modules/treesitter.lua
@@ -3,6 +3,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         lazy = false,
+        branch = "main",
         config = function()
             require('nvim-treesitter').install({
                 'bash',
