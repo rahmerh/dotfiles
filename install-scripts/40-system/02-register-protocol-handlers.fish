701e017228b9e50b3484d975124a67d2434f07a9
Bas <basbouwhuis@protonmail.com>
2026-03-29 00:44:16 +0100

    Set groups

--- install-scripts/40-system/02-register-protocol-handlers.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-xdg-mime default nxm-handler.desktop x-scheme-handler/nxm
--- install-scripts/99-final-message.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-source install-scripts/library/print-utils.fish
-
-print_info "Finished copying dotfiles & running all scripts."
-print_info "It could be necessary to restart the computer in order for all changes to take effect."
-print_success "Have a nice day!"
