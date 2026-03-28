6fdc0f2388542b1053857dc69ba7f62a06acb9af
Bas <basbouwhuis@protonmail.com>
2026-03-29 00:33:44 +0100

    Wallpaper script is now bash

--- home/.config/niri/scripts/set-wallpaper.fish
+++ home/.config/niri/scripts/set-wallpaper.fish
@@ -1,8 +1,8 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
-pkill swaybg >/dev/null 2>&1
+pkill swaybg &>/dev/null
 
-set wp ~/Pictures/wallpaper.*
+wallpapers=("$HOME/Pictures/wallpapers/*")
 
 if set -q wp[1]
     swaybg -i "$wp[1]" -m fill >/dev/null 2>&1 &
