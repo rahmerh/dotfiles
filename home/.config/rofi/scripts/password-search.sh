9a0b6a68bb3c9a9ed9365c1e97b267120d1f8e9f
bas <basbouwhuis@protonmail.com>
2026-06-10 15:51:26 +0200

    Add password search rofi script

---
+++ home/.config/rofi/scripts/password-search.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+passwords_file="$HOME/.cache/password-store/entries.json"
+pass_cli="$HOME/.local/bin/pass-cli"
+
+if [[ ! -r "$passwords_file" ]]; then
+    notify-send "Password search" "No password cache found."
+    exit 1
+fi
+
+if [[ -z "${1:-}" ]]; then
+    jq -r '
+        sort_by(.title | ascii_downcase)[] |
+        [.share_id, .id, .title] |
+        @tsv
+    ' "$passwords_file" |
+        while IFS=$'\t' read -r share_id item_id title; do
+            printf '%s\t%s\0display\x1f%s\x1fmeta\x1f%s\n' \
+                "$share_id" "$item_id" "$title" "$title"
+        done
+    exit 0
+fi
+
+IFS=$'\t' read -r share_id item_id <<<"$1"
+
+password=$("$pass_cli" item view \
+        --share-id "$share_id" \
+        --item-id "$item_id" \
+        --field password)
+
+if [[ -z "$password" ]]; then
+    notify-send "Password search" "Could not retrieve password"
+    exit 1
+fi
+
+wl-copy -o --sensitive "$password"
