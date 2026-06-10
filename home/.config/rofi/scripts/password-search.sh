75fffa2dd7f97bfbf8d615700e984f210fbf4a7c
bas <basbouwhuis@protonmail.com>
2026-06-10 16:01:55 +0200

    Add password store icon for notifications

--- home/.config/rofi/scripts/password-search.sh
+++ home/.config/rofi/scripts/password-search.sh
@@ -2,9 +2,11 @@
 
 passwords_file="$HOME/.cache/password-store/entries.json"
 pass_cli="$HOME/.local/bin/pass-cli"
+notification_icon="$HOME/Pictures/icons/password-key.png"
 
 if [[ ! -r "$passwords_file" ]]; then
-    notify-send "Password search" "No password cache found."
+    notify-send --icon="$notification_icon" \
+        "Password search" "No password cache found."
     exit 1
 fi
 
@@ -29,7 +31,8 @@ password=$("$pass_cli" item view \
         --field password)
 
 if [[ -z "$password" ]]; then
-    notify-send "Password search" "Could not retrieve password"
+    notify-send --icon="$notification_icon" \
+        "Password search" "Could not retrieve password"
     exit 1
 fi
 
